f49edc5b4847c0dccab7dcb7e09ad777bdebe67c
Trayan Manolov <trayan.manolov@stanga.net>
2022-04-21 10:28:53 +0300

    Clean client app

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
-import ABCToken from "./contracts/ABCToken.json";
-import MyTokenSale from "./contracts/MyTokenSale.json";
+import BookLibrary from "./contracts/BookLibrary.json";
 import getWeb3 from "./getWeb3";
 
 import "./App.css";
@@ -20,19 +19,9 @@ class App extends Component {
       this.networkId = await this.web3.eth.net.getId();
 
       this.TokenInstance = new this.web3.eth.Contract(
-        ABCToken.abi,
-        ABCToken.networks[this.networkId] && ABCToken.networks[this.networkId].address
+        BookLibrary.abi,
+        BookLibrary.networks[this.networkId] && BookLibrary.networks[this.networkId].address
       );
-
-      this.TokenSaleInstance = new this.web3.eth.Contract(
-        MyTokenSale.abi,
-        MyTokenSale.networks[this.networkId] && MyTokenSale.networks[this.networkId].address
-      );
-
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.listenToTokenTransfer();
-      this.setState({ loaded: true, tokenSaleAddress: MyTokenSale.networks[this.networkId].address }, this.updateUserTokens);
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -41,22 +30,6 @@ class App extends Component {
       console.error(error);
     }
   };
-
-  updateUserTokens = async () => {
-    let userTokens = await this.TokenInstance.methods.balanceOf(this.accounts[0]).call();
-    this.setState({ userTokens: userTokens });
-  }
-
-  listenToTokenTransfer = () => {
-    this.TokenInstance.events.Transfer({ to: this.accounts[0] }).on("data", this.updateUserTokens);
-  }
-
-  handleMoABCTokensPurchase = async () => {
-    const tokenAmount = this.state.amount;
-    console.log(tokenAmount);
-    await this.TokenSaleInstance.methods.buyTokens(this.accounts[0]).send({ from: this.accounts[0], value: this.web3.utils.toWei(tokenAmount.toString(), "wei") })
-      ;
-  }
 
   handleInputChange = (event) => {
     const target = event.target;
@@ -73,17 +46,8 @@ class App extends Component {
     }
     return (
       <div className="App">
-        <h1>ABC Token Sale</h1>
-        <p>Get your tokens today</p>
-        <p>
-          From the historic tales of Godric, the unmatched prowess of Albus, to the clever charm of Hermione. This house has attained glory unsurpassed and has always provided a light to vanquish the darkness.
-          There is no greater than honour than an official symbol of association with this noble house.
-        </p>
-        <h2>Buy Tokens</h2>
-        <p>If you want to buy tokens, send wei to this address: {this.state.tokenSaleAddress} </p>
-        <p>¥ou currently have {this.state.userTokens} ABC</p>
-        <input type="number" name="amount" min="1" value={this.state.amount} onChange={this.handleInputChange} />
-        <button type="button" onClick={this.handleMoABCTokensPurchase}>Buy More ABC Tokens</button>
+        <h1>Eth Book Library</h1>
+        <p>Simple client for Eth Book Library</p>
       </div>
     );
   }
